8e709656396b81c1275f0eda73cbd7e6f11d714e
williamkirchman-code <williamkirchman@gmail.com>
2026-05-23 11:28:54 -0500

    Delete api/problemas.js

--- api/problemas.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  if (req.method === 'OPTIONS') return res.status(200).end();
-  res.status(200).json([{"materia":"test","tema":"test","subtema":"test","dificultad":"Parcial","enunciado":"Problema de prueba"}]);
-}
